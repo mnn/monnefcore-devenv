b7a2682bf03886b533e116da440d0f0dc0dcc90e
moen <moen@366.hopto.org>
2014-05-27 15:19:42 +0200

    Initial commit (idea project, gradle build file, helper scripts, partial class diagram for net layer).

---
+++ prepareJunction.bat
@@ -0,0 +1,5 @@
+mklink /J src\main\java\monnef\core core\src\main\java\monnef\core
+mklink /J src\main\scala\monnef\core core\src\main\scala\monnef\core
+mklink /J src\main\java\buildcraft core\src\main\java\buildcraft
+
+pause
